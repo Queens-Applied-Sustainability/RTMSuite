9fea5bc80b398bbc5522d54cb6b39ca25a12a927
Philip Schleihauf <uniphil@gmail.com>
2012-12-14 16:56:15 -0500

    add optimizer and some basic tests

--- rtms/optimizer.py
+++ rtms/optimizer.py
@@ -19,4 +19,104 @@
 
     You should have received a copy of the GNU General Public License
     along with RTMSuite.  If not, see <http://www.gnu.org/licenses/>.
+
+    ----
+
+    create an optimization object
+      > static config stuff like lat/lng
+      > reference to the desired rtm class
+      > choose the parameter to optimize for
+      > set up parallelization options
+
+    give it a list of settings, plus a target for the optimizer
+
+    [
+        {
+            'settings': {...},
+            'target': 646.2,
+        },
+        {
+            'settings': {...},
+            'target': 647.7,
+        },
+        ...
+    ]
+
+    it gives back a list of values for the parameter
+
+    [0.1, nan, ...]
+
+    incalculable cases are returned nan.
+
 """
+
+from copy import deepcopy
+import logging
+from numpy import nan
+from fmm import zeroin, BadBoundsError, NoConvergeError
+from rtm import RTMError
+
+
+class Single_Optimizer(object):
+    """
+    Optimize a model for a target irradiance.
+
+    Possible future extension: plug in a custom solver?
+    """
+    
+    def __init__(self, parameter, bounds, tolerance,
+        irradiance='global'):
+        """
+        parameter: a model config setting that the particular rtm supports.
+        bounds: a two-elemnt tuple defining some x which bound the solution.
+        a solution returned will be within +/- tolerance + epsilon of whatever
+        target irradiance is passed to optimize.
+        """
+        self.parameter = parameter
+        self.bounds = bounds
+        self.tolerance = tolerance
+        self.irradiance = irradiance
+
+    def optimize(self, model, target_irradiance):
+        self.meta = {
+            'model': dict(model),
+            'parameter': self.parameter,
+            'target_irradiance': target_irradiance,
+            'iterations': {},
+            }
+
+        def f(x):
+            model.update({self.parameter: x})
+            diff = model.irradiance[self.irradiance] - target_irradiance
+            self.meta['iterations'].update({x: diff})
+            return diff
+
+        result = zeroin(self.bounds[0], self.bounds[1], f, self.tolerance)
+        self.meta['model'].update({self.parameter: result})
+
+        return result
+
+    def clean_up(self):
+        raise NotImplementedError
+
+
+def _optimize(things_list):
+    settings, target, model, optimizer, irradiance, output = things_list
+    model.update(settings)
+    try:
+        answer = optimizer.optimize(model, target_irradiance=target)
+    except (ValueError) as err:
+        logging.error(err)
+        return nan
+    return answer
+
+
+def optimize(settings_list, base_settings, rtm, parameter, map_func=map,
+    tolerance=0.1, bounds=(0,1), irradiance='global', output='aod'):
+    model = rtm(base_settings)
+    optimizer = Single_Optimizer(parameter, bounds, tolerance)
+    things_list = [
+        [item['settings'], item['target'], model, optimizer,
+        irradiance, output] for item in settings_list
+    ]
+    return map_func(_optimize, things_list)
--- rtms/test/test_optimizer.py
+++ rtms/test/test_optimizer.py
@@ -22,13 +22,68 @@
 """
 
 import unittest
-from datetime import datetime
-#from .. import Optimizer
+from dateutil import parser as dtp
+from rtm import SMARTS, RTMError
+from .. import optimizer
+
+base = {'latitude': 39.74, 'longitude': 254.82, 'description': 'test'}
+sample = [
+    {
+        'settings': {'time': dtp.parse('2012-01-01 12:00 -0700')},
+        'target': 420,
+    },
+    {
+        'settings': {'time': dtp.parse('2012-01-01 12:01 -0700')},
+        'target': 421,
+    },
+]
+aod = 'angstroms_coefficient'
+
 
 class TestOptimizer(unittest.TestCase):
 
-    def testX(self):
-        self.assertEqual(1, 1)
+    def assertOptimizedEqual(self, expected, result):
+        self.assertEqual(len(expected), len(result))
+        decimal_places = 1
+        for exp, res in zip(expected, result):
+            self.assertAlmostEqual(exp, res, decimal_places)
+
+    def testFMMInstalled(self):
+        from fmm import zeroin, BadBoundsError, NoConvergeError
+
+    def testSingleSmarts(self):
+        from rtm import SMARTS
+        expected = [0.25]
+        result = optimizer.optimize([sample[0]], base, SMARTS, aod)
+        self.assertOptimizedEqual(expected, result)
+
+    def testSingleSmartsMultiprocessing(self):
+        from multiprocessing import Pool
+        from rtm import SMARTS
+        expected = [0.25]
+        result = optimizer.optimize([sample[0]], base, SMARTS, aod, Pool().map)
+        self.assertOptimizedEqual(expected, result)
+
+    def testSmartsMultiprocessing(self):
+        from multiprocessing import Pool
+        from rtm import SMARTS
+        expected = [0.25, 0.24]
+        result = optimizer.optimize(sample, base, SMARTS, aod, Pool(2).map)
+        self.assertOptimizedEqual(expected, result)
+
+    def testSBdartMultiprocessing(self):
+        from multiprocessing import Pool
+        from rtm import SBdart
+        expected = [0.25, 0.24]
+        result = optimizer.optimize(sample, base, SBdart, aod, Pool(1).map)
+        self.assertOptimizedEqual(expected, result)
+
+    @classmethod
+    def tearDownClass(cls):
+        from shutil import rmtree
+        test_dir_name = '-'.join([base['description'],
+            str(base['longitude']), str(base['latitude'])])
+        rmtree(test_dir_name)
 
 
 if __name__ == '__main__':
