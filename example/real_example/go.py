7c5472b12fe91a4bfb417cf4606092ee8b1655fa
Philip Schleihauf <uniphil@gmail.com>
2013-01-21 17:05:56 -0500

    ellipses

--- example/real_example/go.py
+++ example/real_example/go.py
@@ -4,17 +4,16 @@ CONFIG_FILE = open('config.yaml')
 DATA_FILE = open('time-series.csv')
 
 
-print "importing configuration"
-# import the config
+print "importing configuration..."
 site_info, csv_map, run_config = rtms.importer.config(CONFIG_FILE)
 
 
-print "importing data"
-# get the data
+print "importing data..."
 time_series_in = rtms.importer.data(DATA_FILE, csv_map)
 
 
-print "selecting clear days"
+print "selecting clear days..."
 selector = rtms.Selector(site_info['latitude'], site_info['longitude'])
 time_irrad_clear = selector.select(time_series_in[['time', 'irradiance']])
+
 
